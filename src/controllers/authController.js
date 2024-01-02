11453bce8d3d8a863abef9e11e579db546ba3df9
Shreyosi Sarkar <21f1003968@ds.study.iitm.ac.in>
2024-01-03 00:14:12 +0530

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,16 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const token = require('../middlewares/token');
 
+const validateUser = async function(password, hash) {
+    await bcrypt.compare(password, hash)
+                .then((result) => {
+                    return result;
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+}
+
 const register = async function (req, res) {
     try {
         let errors = {};
@@ -29,6 +39,19 @@ const register = async function (req, res) {
                 });
             }
         }
+
+        const existingEmail = await User.findOne({
+            email: email,
+        });
+        const existingPhone = await User.findOne({
+            phone: phone,
+        });
+        if (existingEmail || existingPhone) {
+            res.status(400).json({
+                message: "User already exists"
+            });
+            return;
+        };
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -59,9 +82,72 @@ const register = async function (req, res) {
     }
 };
 
-const login = function (req, res) {
+const login = async function (req, res) {
+    try {
+        let errors = {};
+        const { email, phone, password } = req.body;
 
-};
+        ['email', 'phone', 'password'].forEach(key => {
+            if (req.body[key] === null || req.body[key] === undefined) {
+                errors[key] = `${key} is a required parameter`;
+            }
+            else if (req.body[key] === "") {
+                errors[key] = `${key} cannot be empty`;
+            }
+        });
+
+        if (_.keys(errors).length > 0) {
+            if (_.hasIn(errors, 'email') && _.hasIn(errors, 'phone')) {
+                return res.status(400).json({
+                    message: 'Provide one of the email or phone.'
+                });
+            }
+            else if (_.hasIn(errors, 'password')) {
+                return res.status(400).json({
+                    message: errors['password']
+                });
+            }
+        }
+        else {
+            return res.status(400).json({
+                message: 'Provide only one of the email or phone'
+            })
+        };
+
+        const user = await User.findOne({
+            email: email ? email : "",
+            phone: phone ? phone : ""
+        });
+        if (user) {
+            if (validateUser(password, user.password)) {
+                res.status(200).json({
+                    success: true,
+                    data: {
+                        email: user.email,
+                        phone: user.phone,
+                        token: token.generateAccessToken(user._id.toJSON())
+                    }
+                });
+            }
+            else {
+                res.status(401).json({
+                    message: "Incorrect Credentials"
+                });
+            }
+        }
+        else {
+            res.status(401).json({
+                message: "Incorrect Credentials"
+            });
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+}
 
 module.exports = {
     register,
